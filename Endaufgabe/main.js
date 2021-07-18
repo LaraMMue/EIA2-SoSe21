350032b6ed29a8ac2187fc0782949de288fa04bc
LaraMMue <lamamue1@gmx.de>
2021-07-18 15:39:23 +0200

    Spieler auwechseln

--- Endaufgabe/main.js
+++ Endaufgabe/main.js
@@ -10,11 +10,10 @@ var endaufgabe_jogi;
     endaufgabe_jogi.checkArrival = true;
     endaufgabe_jogi.width = Number(endaufgabe_jogi.canvas.width);
     endaufgabe_jogi.height = Number(endaufgabe_jogi.canvas.height);
-    /*let positionsA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
-    let positionsB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)];
-    let baseA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
-    let baseB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)];
-    */
+    //let positionsA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
+    //let positionsB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)];
+    //let baseA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
+    //let baseB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)]; 
     let TASK;
     (function (TASK) {
         TASK[TASK["WAIT"] = 0] = "WAIT";
@@ -37,7 +36,8 @@ var endaufgabe_jogi;
     let precisionMin = 5;
     let precisionMaxA = Number("");
     let precisionMaxB = Number("");
-    let playerNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let playerNumbersA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let playerNumbersB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     let menu = document.querySelector("#settings");
     let startbtn = document.querySelector("#start");
     let newGameBtn = document.querySelector("#newGame");
@@ -63,8 +63,8 @@ var endaufgabe_jogi;
             reachBall(e.detail);
         });
         endaufgabe_jogi.canvas.addEventListener("click", handleClick);
-        //document.addEventListener("keydown", handleExchange);
-        //canvas.addEventListener("click", handleInfo);
+        document.addEventListener("keydown", replacePlayer);
+        endaufgabe_jogi.canvas.addEventListener("click", displayPlayerInfo);
         endaufgabe_jogi.drawField();
         //drawPlayer();
         //drawBall();
@@ -103,14 +103,14 @@ var endaufgabe_jogi;
     }
     function createTeamA() {
         for (let i = 0; i < 11; i++) {
-            let playerA = new endaufgabe_jogi.Player(positionsA[i], baseA[i], teamA, createRandomNum(speedMin, speedMaxA), createRandomNum(precisionMin, precisionMaxA), colorA, playerNumbers[i]);
+            let playerA = new endaufgabe_jogi.Player(positionsA[i], baseA[i], teamA, createRandomNum(speedMin, speedMaxA), createRandomNum(precisionMin, precisionMaxA), colorA, playerNumbersA[i]);
             playerA.draw();
             moveables.push(playerA);
         }
     }
     function createTeamB() {
         for (let i = 0; i < 11; i++) {
-            let playerB = new endaufgabe_jogi.Player(positionsB[i], baseB[i], teamB, createRandomNum(speedMin, speedMaxB), createRandomNum(precisionMin, precisionMaxB), colorB, playerNumbers[i]);
+            let playerB = new endaufgabe_jogi.Player(positionsB[i], baseB[i], teamB, createRandomNum(speedMin, speedMaxB), createRandomNum(precisionMin, precisionMaxB), colorB, playerNumbersB[i]);
             playerB.draw();
             moveables.push(playerB);
         }
@@ -172,8 +172,7 @@ var endaufgabe_jogi;
             let vector1 = new endaufgabe_jogi.Vector(player[i].position.x, player[i].position.y);
             let vector2 = new endaufgabe_jogi.Vector(ball[0].position.x, ball[0].position.y);
             let difference = endaufgabe_jogi.Vector.getDifference(vector1, vector2);
-            let length = difference.length;
-            if (length <= endaufgabe_jogi.canvas.width / 110 * 30) {
+            if (difference.length <= endaufgabe_jogi.canvas.width / 110 * 30) {
                 player[i].changeTask(TASK.MOVE, ball[0].position);
             }
         }
@@ -185,6 +184,79 @@ var endaufgabe_jogi;
         animation = true;
         endaufgabe_jogi.checkArrival = true;
         event = _event;
+    }
+    function replacePlayer(_event) {
+        let keyPressed = _event.key;
+        if (keyPressed == "a") {
+            endaufgabe_jogi.canvas.addEventListener("click", newPlayerA);
+        }
+        else if (keyPressed == "b") {
+            endaufgabe_jogi.canvas.addEventListener("click", newPlayerB);
+        }
+        else if (keyPressed == "-") {
+            endaufgabe_jogi.canvas.addEventListener("click", deletePlayer);
+        }
+    }
+    function newPlayerA(_event) {
+        let altPressed = _event.altKey;
+        if (altPressed == true) {
+            let rect = endaufgabe_jogi.canvas.getBoundingClientRect();
+            let x = _event.clientX - rect.left;
+            let y = _event.clientY - rect.top;
+            baseA.push(new endaufgabe_jogi.Vector(x, y));
+            playerNumbersA.push(1);
+            let newPlayer = new endaufgabe_jogi.Player(new endaufgabe_jogi.Vector(x, y), baseA[baseA.length - 1], teamA, createRandomNum(speedMin, speedMaxA), createRandomNum(precisionMin, precisionMaxA), colorA, playerNumbersA.length);
+            moveables.push(newPlayer);
+            endaufgabe_jogi.canvas.removeEventListener("click", newPlayerA);
+        }
+    }
+    function newPlayerB(_event) {
+        let altPressed = _event.altKey;
+        if (altPressed == true) {
+            let rect = endaufgabe_jogi.canvas.getBoundingClientRect();
+            let x = _event.clientX - rect.left;
+            let y = _event.clientY - rect.top;
+            baseB.push(new endaufgabe_jogi.Vector(x, y));
+            playerNumbersB.push(1);
+            let newPlayer = new endaufgabe_jogi.Player(new endaufgabe_jogi.Vector(x, y), baseB[baseB.length - 1], teamB, createRandomNum(speedMin, speedMaxB), createRandomNum(precisionMin, precisionMaxB), colorB, playerNumbersB.length);
+            moveables.push(newPlayer);
+            endaufgabe_jogi.canvas.removeEventListener("click", newPlayerB);
+        }
+    }
+    function deletePlayer(_event) {
+        let ctrlPressed = _event.ctrlKey;
+        if (ctrlPressed == true) {
+            let rect = endaufgabe_jogi.canvas.getBoundingClientRect();
+            let x = _event.clientX - rect.left;
+            let y = _event.clientY - rect.top;
+            for (let [i, moveable] of moveables.entries()) {
+                if (moveable instanceof endaufgabe_jogi.Player) {
+                    let vector1 = new endaufgabe_jogi.Vector(moveable.position.x, moveable.position.y);
+                    let vector2 = new endaufgabe_jogi.Vector(x, y);
+                    let difference = endaufgabe_jogi.Vector.getDifference(vector1, vector2);
+                    if (difference.length <= endaufgabe_jogi.canvas.width / 110 * 5) {
+                        moveables.splice(i, 1);
+                    }
+                }
+            }
+        }
+    }
+    function displayPlayerInfo(_event) {
+        let shiftPressed = _event.shiftKey;
+        if (shiftPressed == true) {
+            for (let moveable of moveables) {
+                if (moveable instanceof endaufgabe_jogi.Player) {
+                    let rect = endaufgabe_jogi.canvas.getBoundingClientRect();
+                    let x = _event.clientX - rect.left;
+                    let y = _event.clientY - rect.top;
+                    let mousePos = new endaufgabe_jogi.Vector(x, y);
+                    let difference = endaufgabe_jogi.Vector.getDifference(moveable.position, mousePos);
+                    if (difference.length / 100 < 1) {
+                        moveable.playerInformation(_event);
+                    }
+                }
+            }
+        }
     }
 })(endaufgabe_jogi || (endaufgabe_jogi = {}));
 //# sourceMappingURL=main.js.map
--- Endaufgabe/main.ts
+++ Endaufgabe/main.ts
@@ -16,11 +16,11 @@ namespace endaufgabe_jogi {
     export let width: number = Number (canvas.width);
     export let height: number = Number (canvas.height);
 
-    /*let positionsA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
-    let positionsB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)];
-    let baseA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
-    let baseB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)]; 
-    */
+    //let positionsA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
+    //let positionsB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)];
+    //let baseA: Vector [] = [new Vector(width / 110 * 10, height / 2 + 30), new Vector(width / 110 * 15, height / 75 * 17), new Vector (width / 110 * 15, height / 75 * 65), new Vector(width / 110 * 32, height / 2 + 30), new Vector(width / 110 * 43, height / 2 - 110), new Vector(width / 110 * 43, height / 2 + 180), new Vector(width / 110 * 57, height / 2 + 125), new Vector(width / 110 * 75, height / 75 * 15), new Vector(width / 110 * 75, height / 75 * 68), new Vector(width / 110 * 88.5, height / 2 - 50), new Vector(width / 110 * 88.5, height / 2 + 110)];
+    //let baseB: Vector [] = [new Vector(width / 110 * 100, height / 2 + 30), new Vector(width / 110 * 78, height / 2 + 30), new Vector(width / 110 * 67, height / 2 - 110), new Vector(width / 110 * 67, height / 2 + 180), new Vector(width / 110 * 54, height / 2 - 60), new Vector(width / 110 * 35, height / 75 * 68), new Vector(width / 110 * 21.5, height / 2 + 110), new Vector(width / 110 * 21.5, height / 2 - 50), new Vector(width / 110 * 35, height / 75 * 15), new Vector(width / 110 * 95, height / 75 * 65), new Vector(width / 110 * 95, height / 75 * 17)]; 
+    
 
     export enum TASK {
         WAIT,
@@ -50,7 +50,8 @@ namespace endaufgabe_jogi {
     let precisionMaxA: number = Number("");
     let precisionMaxB: number = Number("");
 
-    let playerNumbers: number [] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let playerNumbersA: number [] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    let playerNumbersB: number [] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
     let menu: HTMLDivElement = <HTMLDivElement> document.querySelector("#settings");
 
@@ -96,8 +97,8 @@ namespace endaufgabe_jogi {
 
         canvas.addEventListener("click", handleClick);
 
-        //document.addEventListener("keydown", handleExchange);
-        //canvas.addEventListener("click", handleInfo);
+        document.addEventListener("keydown", replacePlayer);
+        canvas.addEventListener("click", displayPlayerInfo);
 
 
 
@@ -152,7 +153,7 @@ namespace endaufgabe_jogi {
 
     function createTeamA(): void {
         for (let i: number = 0; i < 11; i ++) {
-            let playerA: Player = new Player(positionsA[i], baseA[i], teamA, createRandomNum(speedMin, speedMaxA), createRandomNum(precisionMin, precisionMaxA), colorA, playerNumbers[i]);
+            let playerA: Player = new Player(positionsA[i], baseA[i], teamA, createRandomNum(speedMin, speedMaxA), createRandomNum(precisionMin, precisionMaxA), colorA, playerNumbersA[i]);
             playerA.draw();
             moveables.push(playerA);
         }
@@ -160,7 +161,7 @@ namespace endaufgabe_jogi {
 
     function createTeamB(): void {
         for (let i: number = 0; i < 11; i ++) {
-            let playerB: Player = new Player(positionsB[i], baseB[i], teamB, createRandomNum(speedMin, speedMaxB), createRandomNum(precisionMin, precisionMaxB), colorB, playerNumbers[i]);
+            let playerB: Player = new Player(positionsB[i], baseB[i], teamB, createRandomNum(speedMin, speedMaxB), createRandomNum(precisionMin, precisionMaxB), colorB, playerNumbersB[i]);
             playerB.draw();
             moveables.push(playerB);
         }
@@ -239,8 +240,7 @@ namespace endaufgabe_jogi {
             let vector1: Vector = new Vector(player[i].position.x, player[i].position.y);
             let vector2: Vector = new Vector(ball[0].position.x, ball[0].position.y);
             let difference: Vector = Vector.getDifference(vector1, vector2);
-            let length: number = difference.length;
-            if (length <= canvas.width / 110 * 30) {
+            if (difference.length <= canvas.width / 110 * 30) {
                 player[i].changeTask(TASK.MOVE, ball[0].position);
             }
         }
@@ -255,6 +255,99 @@ namespace endaufgabe_jogi {
         checkArrival = true;
         event = _event;
     }
+
+    function replacePlayer(_event: KeyboardEvent): void {
+        let keyPressed: string = _event.key;
+
+        if (keyPressed == "a") {
+            canvas.addEventListener("click", newPlayerA);
+        }
+
+        else if (keyPressed == "b") {
+            canvas.addEventListener("click", newPlayerB);
+        }
+
+        else if (keyPressed == "-") {
+            canvas.addEventListener("click", deletePlayer);
+        }
+    }
+
+    function newPlayerA(_event: MouseEvent): void {
+        let altPressed: boolean = _event.altKey;
+
+        if (altPressed == true) {
+            let rect: DOMRect = canvas.getBoundingClientRect();
+            let x: number = _event.clientX - rect.left;
+            let y: number = _event.clientY - rect.top;
+            baseA.push(new Vector(x, y));
+            playerNumbersA.push(1);
+            let newPlayer: Player = new Player(new Vector(x, y), baseA[baseA.length - 1], teamA, createRandomNum(speedMin, speedMaxA), createRandomNum(precisionMin, precisionMaxA), colorA, playerNumbersA.length);
+            moveables.push(newPlayer);
+            canvas.removeEventListener("click", newPlayerA);
+            
+        }
+    }
+
+    function newPlayerB(_event: MouseEvent): void {
+        let altPressed: boolean = _event.altKey;
+
+        if (altPressed == true) {
+            let rect: DOMRect = canvas.getBoundingClientRect();
+            let x: number = _event.clientX - rect.left;
+            let y: number = _event.clientY - rect.top;
+            baseB.push(new Vector(x, y));
+            playerNumbersB.push(1);
+            let newPlayer: Player = new Player(new Vector(x, y), baseB[baseB.length - 1], teamB, createRandomNum(speedMin, speedMaxB), createRandomNum(precisionMin, precisionMaxB), colorB, playerNumbersB.length);
+            moveables.push(newPlayer);
+            canvas.removeEventListener("click", newPlayerB);
+            
+        }
+    }
+
+    function deletePlayer(_event: MouseEvent): void {
+        let ctrlPressed: boolean = _event.ctrlKey;
+
+        if (ctrlPressed == true) {
+            let rect: DOMRect = canvas.getBoundingClientRect();
+            let x: number = _event.clientX - rect.left;
+            let y: number = _event.clientY - rect.top;
+
+            for (let [i, moveable] of moveables.entries()) {
+                if (moveable instanceof Player) {
+                    let vector1: Vector = new Vector(moveable.position.x, moveable.position.y);
+                    let vector2: Vector = new Vector(x, y);
+                    let difference: Vector = Vector.getDifference(vector1, vector2);
+                    if (difference.length <= canvas.width / 110 * 5) {
+                        moveables.splice(i, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    function displayPlayerInfo(_event: MouseEvent): void {
+        let shiftPressed: boolean = _event.shiftKey;
+
+        if (shiftPressed == true) {
+            for (let moveable of moveables) {
+                if (moveable instanceof Player) {
+                    let rect: DOMRect = canvas.getBoundingClientRect();
+                    let x: number = _event.clientX - rect.left;
+                    let y: number = _event.clientY - rect.top;
+                    let mousePos: Vector = new Vector(x, y);
+                    let difference: Vector = Vector.getDifference(moveable.position, mousePos);
+
+                    if (difference.length / 100 < 1) {
+                        moveable.playerInformation(_event);
+                    }
+                }
+            }
+        }
+    }
+    
+    
+
+
 
     
 
